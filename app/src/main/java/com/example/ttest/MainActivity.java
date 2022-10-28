63a01cad430f39fc05fe5a1b1e73ebe834b56034
Paulina Hudzik <paulinanunab@gmail.com>
2022-10-28 21:53:14 +0200

    test latarki

--- app/src/main/java/com/example/ttest/MainActivity.java
+++ app/src/main/java/com/example/ttest/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.ttest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button_latarka; //activity_main.xml -> activity_latarka.xml
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE},
+                PackageManager.PERMISSION_GRANTED);
+
+
+        //activity_main.xml -> activity_latarka.xml przez przycisk
+        button_latarka = (Button) findViewById(R.id.button_latarka);
+
+        button_latarka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,latarka.class);
+                startActivity(intent);
+            }
+        }); //koniec activity_main.xml -> activity_latarka.xml przez przycisk
+
+
     }
 }
+
---
+++ app/src/main/java/com/example/ttest/latarka.java
@@ -0,0 +1,77 @@
+package com.example.ttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+
+public class latarka extends AppCompatActivity {
+
+    private ImageButton toggle_latarka;
+
+    boolean hasCameraFlash = false;
+    boolean flashOn = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latarka);
+
+        toggle_latarka = findViewById(R.id.toggle_latarka);
+
+        hasCameraFlash = getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        toggle_latarka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(hasCameraFlash){
+                    if(flashOn){
+                        flashOn = false;
+                        flashLightOff();
+                    }
+                    else
+                    {
+                        flashOn = true;
+                        flashLightOn();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(latarka.this, "No flash available on your device", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+        private void flashLightOn(){
+            CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            try{
+                assert cameraManager != null;
+                String cameraId = cameraManager.getCameraIdList()[0];
+                cameraManager.setTorchMode(cameraId, true);
+                Toast.makeText(latarka.this, "Latarka jest włączona", Toast.LENGTH_SHORT).show();
+            }
+            catch(CameraAccessException e){
+                Log.e("Camera Problem", "Nie można włączyć latarki");
+            }
+        }
+
+    private void flashLightOff(){
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try{
+            assert cameraManager != null;
+            String cameraId = cameraManager.getCameraIdList()[0];
+            cameraManager.setTorchMode(cameraId, false);
+            Toast.makeText(latarka.this, "Latarka jest wyłączona", Toast.LENGTH_SHORT).show();
+        }
+        catch(CameraAccessException e){
+            Log.e("Camera Problem", "Nie można włączyć latarki");
+        }
+    }
+}
