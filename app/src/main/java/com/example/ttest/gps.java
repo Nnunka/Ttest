99aee0647da57f9d3eb7235509a3ce9a38078722
Paulina Hudzik <paulinanunab@gmail.com>
2022-11-13 12:35:31 +0100

    gps przez współrzędne v2

--- app/src/main/java/com/example/ttest/gps.java
+++ app/src/main/java/com/example/ttest/gps.java
@@ -1,14 +1,106 @@
 package com.example.ttest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class gps extends AppCompatActivity {
+
+public class gps extends AppCompatActivity implements IBaseGpsListener {
+
+    private static final int PERMISSION_LOCATION = 1000;
+
+    TextView tv_location;
+    Button b_location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps);
+
+        tv_location = findViewById(R.id.tv_location);
+        b_location = findViewById(R.id.b_location);
+
+        b_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_LOCATION);
+                }
+                else {
+                    showLocation();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showLocation();
+            } else {
+                Toast.makeText(this, "Nie ma dostępu!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void showLocation() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            tv_location.setText("Znajduję lokalizacje...");
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
+        else {
+            Toast.makeText(this, "Włącz lokalizacje!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+        }
+    }
+
+    private String hereLocation(Location location) {
+        return "Szerokość: " + location.getLatitude() + "\nDługość: " + location.getLongitude();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //update
+        tv_location.setText(hereLocation(location));
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        //empty
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        //empty
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        //empty
+    }
+
+    @Override
+    public void onGpsStatusChanged(int event) {
+        //empty
     }
 }
