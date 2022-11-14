a1cd1d3effd1dd7a34d99734df20692801e0a821
Paulina Hudzik <paulinanunab@gmail.com>
2022-11-14 21:08:06 +0100

    test dźwięku

--- app/src/main/java/com/example/ttest/dzwiek.java
+++ app/src/main/java/com/example/ttest/dzwiek.java
@@ -2,7 +2,10 @@ package com.example.ttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class dzwiek extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class dzwiek extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dzwiek);
+
+        final MediaPlayer sound = MediaPlayer.create(this, R.raw.dzwiek_audio);
+
+        Button btn_dzwiek = findViewById(R.id.btn_dzwiek);
+
+        btn_dzwiek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               sound.start();
+            }
+        });
     }
 }
