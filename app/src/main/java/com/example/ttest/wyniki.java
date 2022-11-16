516b08d14a734065248701aaefe1c316deb8afc7
Magdalena Krzyszowska <magda.krzyszowska@onet.pl>
2022-11-16 16:14:53 +0100

    dodanie nowego activity w którym będą znajdować się wyniki testów

---
+++ app/src/main/java/com/example/ttest/wyniki.java
@@ -0,0 +1,14 @@
+package com.example.ttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class wyniki extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wyniki);
+    }
+}
