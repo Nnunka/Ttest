ccdb6728cd38d20731daab6e86aaf1f1c3093419
Paulina Hudzik <paulinanunab@gmail.com>
2022-12-05 20:14:26 +0100

    aktualizacja testu wifi, wyświetla adres ip, mac routera, ssid

--- app/src/main/java/com/example/ttest/wifi.java
+++ app/src/main/java/com/example/ttest/wifi.java
@@ -2,72 +2,53 @@ package com.example.ttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
+import android.annotation.SuppressLint;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.widget.CompoundButton;
-import android.widget.Switch;
+import android.text.format.Formatter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.List;
 
 public class wifi extends AppCompatActivity {
 
-    private Switch wifiSwitch;
-    private WifiManager wifiManager;
+    TextView txtWifiInfo;
+    Button btnInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi);
 
-        wifiSwitch = findViewById(R.id.wifi_switch);
-        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        wifiManager.setWifiEnabled(true);
+        txtWifiInfo = (TextView) findViewById(R.id.idTxt);
+        btnInfo = (Button) findViewById(R.id.idBtn);
 
-        wifiSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    wifiManager.setWifiEnabled(true);
-                    wifiSwitch.setText("WiFi is ON");
-                } else {
-                    wifiManager.setWifiEnabled(false);
-                    wifiSwitch.setText("WiFi is OFF");
-                }
-            }
-        });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        IntentFilter intentFilter = new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION);
-        registerReceiver(wifiStateReceiver, intentFilter);
+    public void getWifiInformation(View view) {
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+
+        int ip = wifiInfo.getIpAddress();
+        String ipAddress = Formatter.formatIpAddress(ip);
+        String bssid = wifiInfo.getBSSID();
+        String ssid = wifiInfo.getSSID();
+        int rssi = wifiInfo.getRssi();
+
+
+        String info =
+                "\n Adres IP: " + ipAddress +
+                "\n Adres MAC Routera: " + bssid +
+                "\n SSID: " + ssid +
+                "\n Wskaźnik mocy: " + rssi;
+
+        txtWifiInfo.setText(info);
+
     }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        unregisterReceiver(wifiStateReceiver);
-    }
-
-    private BroadcastReceiver wifiStateReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            int wifiStateExtra = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE,
-                    WifiManager.WIFI_STATE_UNKNOWN);
-
-            switch (wifiStateExtra) {
-                case WifiManager.WIFI_STATE_ENABLED:
-                    wifiSwitch.setChecked(true);
-                    wifiSwitch.setText("WiFi is ON");
-                    break;
-                case WifiManager.WIFI_STATE_DISABLED:
-                    wifiSwitch.setChecked(false);
-                    wifiSwitch.setText("WiFi is OFF");
-                    break;
-            }
-        }
-    };
 }
